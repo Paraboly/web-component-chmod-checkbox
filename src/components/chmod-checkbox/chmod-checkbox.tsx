4bcb97e496f100fc06af7c03028a7eb66acbe2c4
FreakyCoder <kurayogun@gmail.com>
2019-10-01 17:42:29 +0300

    Two-way binding for permission field

--- src/components/chmod-checkbox/chmod-checkbox.tsx
+++ src/components/chmod-checkbox/chmod-checkbox.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, h, State } from "@stencil/core";
+import { Component, Prop, h } from "@stencil/core";
 import { calculatePermission } from "../../utils/utils";
 
 @Component({
@@ -22,7 +22,7 @@ export class CHModCheckbox {
    */
   @Prop() last: string = "EXECUTE";
 
-  @State() permission: number = 0;
+  @Prop({ mutable: true, reflect: true }) permission: number = 0;
   @Prop() isReadChecked: boolean = false;
   @Prop() isWriteChecked: boolean = false;
   @Prop() isExecuteChecked: boolean = false;
